7cf317e5c8fa78e7e5e9e711c6e68f5ed9122e5d
dpizapal <pizarro.developer@gmail.com>
2022-11-16 11:09:30 +0100

    to StrictEqual y toEqual

---
+++ src/tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,31 @@
+import { getUser } from "../../base-pruebas/05-funciones";
+import { getUsuarioActivo } from "../../base-pruebas/05-funciones";
+
+describe('Pruebas en 05-funciones', () => {
+  test('getUser debe de retornar un objeto ', () => {
+    
+    const testUser = {
+      uid: 'ABC123',
+      username: 'El_Papi1502'
+    };
+    
+    const user = getUser();
+  
+    expect( testUser ).toEqual ( user );
+
+  });
+
+  test('getUsuarioActivo debe retornar un objeto', () => {
+    
+    const name = 'Fernando';
+    const user = getUsuarioActivo( name );
+
+    expect( user ).toStrictEqual ({
+      uid: 'ABC567',
+      username: name
+    });
+
+  });
+  
+  
+});
