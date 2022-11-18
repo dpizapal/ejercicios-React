03ac788de0718f3a8823a4ee210e4737f2a1a7fa
dpizapal <pizarro.developer@gmail.com>
2022-11-18 13:22:56 +0100

    catch en async await

--- src/base-pruebas/11-async-await.js
+++ src/base-pruebas/11-async-await.js
@@ -15,7 +15,7 @@ export const getImagen = async() => {
 
     } catch (error) {
         // manejo del error
-        console.error(error) 
+        //console.error(error) 
         return 'No se encontró la imagen'
     }
     
--- src/tests/base-pruebas/11-async-await.test.js
+++ src/tests/base-pruebas/11-async-await.test.js
@@ -3,11 +3,12 @@ import {getImagen} from '../../base-pruebas/11-async-await'
 
 describe('Pruebas en 11-async-await', () => { 
     
-    test('getImagen debe retornar un url de la imagen ', async() => {
+    test('getImagen debe retornar un error ', async() => {
       
-       const url = await getImagen();
+       const resp = await getImagen();
 
-       expect ( typeof url ).toBe('string');
+       //expect ( typeof url ).toBe('string');
+       expect ( resp ).toBe('No se encontró la imagen');
 
     });
      
