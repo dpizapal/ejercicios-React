36bd22043870070188e89716b1bff3d3542aeb2c
dpizapal <pizarro.developer@gmail.com>
2022-11-18 19:10:11 +0100

    testing en react

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -7,21 +7,23 @@ import Proptypes from 'prop-types'
 
 
 
-export const FirstApp = ({title, subtitle}) => {
+export const FirstApp = ({title, subtitle, name}) => {
 
   
   return (
     <>
     <h1>{title}</h1>
     <p>{subtitle}</p>
+    <p>{name}</p>
     </>
   )
 }
 FirstApp.propTypes = {
   title: Proptypes.string.isRequired,
-  subtitle: Proptypes.number.isRequired,
+  subtitle: Proptypes.string.isRequired,
 }
 FirstApp.defaultProps = {
   title: 'No hay titulo',
+  subtitle: 'No hay titulo',
   name: 'David Pizarro'
 }
---
+++ src/tests/FirstApp.test.jsx
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import { FirstApp } from "../FirstApp";
+
+
+
+describe('Pruebas en <FirstApp />', () => { 
+
+  test('debe de hacer match con el snapshot', () => { 
+
+    const title = 'Hola, soy Goku'
+    render ( <FirstApp title = { title }/> )
+
+   })
+
+ })
