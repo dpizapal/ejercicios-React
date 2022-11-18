ef71746858bba122689d00a4a5fe9ffc6d45d685
dpizapal <pizarro.developer@gmail.com>
2022-11-18 13:13:56 +0100

    watchwg-fetch instalado

---
+++ src/tests/base-pruebas/11-async-await.test.js
@@ -0,0 +1,17 @@
+import { string } from 'prop-types';
+import {getImagen} from '../../base-pruebas/11-async-await'
+
+describe('Pruebas en 11-async-await', () => { 
+    
+    test('getImagen debe retornar un url de la imagen ', async() => {
+      
+       const url = await getImagen();
+
+       expect ( typeof url ).toBe('string');
+
+    });
+     
+
+})
+
+
